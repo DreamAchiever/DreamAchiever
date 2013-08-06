fc4cb5c5b9c681776e0e72be4ce152bff8029f75
MatrixWeb <454033517@qq.com>
2013-08-06 18:11:03 +0800

    A
    1

---
+++ Authenticate/views.py
@@ -0,0 +1,30 @@
+# Create your views here.
+from django.http import HttpRequest
+# from models import User,Permission
+# def login(req):
+#     req=HttpRequest()
+#     user=User.check(req['username'],req['password'])
+#     if user!=None:
+#         permissions= Permission.objects.filter(role__user__id=user.id)
+# def logout(req):
+# 
+# def _login_(req,user,permissions):
+#     req.session()
+#     return short_cut("calendar/calendar.html",{'news':news})
+# def _get_user_(req):
+#     return req.session().get('')
+# 
+# def _get_permissions_(req):
+#     return req.session().get('')
+# def checkPermission(req):
+#     path=req.path
+#     permissions=_get_permissions_(req)
+#     for permission in permissions:
+#         if permission.url.equal(path):
+#             return True
+#     return False
+# def add(req):
+#     event=Event()
+#     event.
+#     
+
