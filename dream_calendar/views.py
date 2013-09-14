2d398bdc766d51d4746491bcde12cc25d5dae3f1
00wuyu00 <a7210316@gmail.com>
2013-09-14 17:19:50 +0800

    go

--- dream_calendar/views.py
+++ dream_calendar/views.py
@@ -4,7 +4,7 @@ from django.http.response import HttpResponse
 from dream_calendar.models import Subscription
 
 #获取当前用户的所有日历
-@url_deco
+@url_deco(url="sdfjksdlfl")
 def get_calendars_by_login_user(request):
     login_user = request.session.get("login_user",None)
     if login_user :
